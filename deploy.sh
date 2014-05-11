f43f0ac24bc7166f41d29b797e5333a103992d19
Moose <moose@posteo.de>
2014-05-11 12:32:25 +0000

    add config files for automatic bluetooth play start

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,14 @@ EXEC_FILES=(
 	stations.py
 	)
 
+# make sure directory structure exists
+if [ ! -d /opt/paradium ]; then
+	mkdir -p /opt/paradium
+fi
+if [ ! -d /opt/paradium/scripts ]; then
+	mkdir -p /opt/paradium/scripts
+fi
+
 for i in "${EXEC_FILES[@]}"
 	do
 		echo "copying "$i" to /opt/paradium ..."
@@ -17,4 +25,12 @@ for i in "${EXEC_FILES[@]}"
 echo "sync htdocs ..."
 rsync -av htdocs /opt/paradium/
 
+echo "copy udev start rule ..."
+sudo cp -a ./udev/98-bluez-pulse.rules /etc/udev/rules.d/98-bluez-pulse.rules
+
+echo "copy bluetooth autostart on connect script ..."
+cp -a ./scripts/bluez-pulse-plugged /opt/paradium/scripts/bluez-pulse-plugged
+chmod +x /opt/paradium/scripts/bluez-pulse-plugged
+
+echo ".. all done!"
 
