0df6bd779c201f09f1f20150abcd3cc58bcbe173
Moose <moose@posteo.de>
2014-05-11 12:33:56 +0000

    end of options markers

--- deploy.sh
+++ deploy.sh
@@ -19,17 +19,17 @@ fi
 for i in "${EXEC_FILES[@]}"
 	do
 		echo "copying "$i" to /opt/paradium ..."
-		cp -a "$i" /opt/paradium
+		cp -a -- "$i" /opt/paradium
 	done
 
 echo "sync htdocs ..."
-rsync -av htdocs /opt/paradium/
+rsync -av -- htdocs /opt/paradium/
 
 echo "copy udev start rule ..."
-sudo cp -a ./udev/98-bluez-pulse.rules /etc/udev/rules.d/98-bluez-pulse.rules
+sudo cp -a -- ./udev/98-bluez-pulse.rules /etc/udev/rules.d/98-bluez-pulse.rules
 
 echo "copy bluetooth autostart on connect script ..."
-cp -a ./scripts/bluez-pulse-plugged /opt/paradium/scripts/bluez-pulse-plugged
+cp -a -- ./scripts/bluez-pulse-plugged /opt/paradium/scripts/bluez-pulse-plugged
 chmod +x /opt/paradium/scripts/bluez-pulse-plugged
 
 echo ".. all done!"
